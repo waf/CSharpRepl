23e90f4c52bb207a497608bbaa761ef11eedf59e
Will Fuqua <wafuqua@gmail.com>
2022-05-07 20:18:38 +0700

    Handle exceptions in the pretty printer
    Sometimes the roslyn CSharpObjectFormatter throws exceptions. We've
    seen this with ref structs and some other cases. For example
    Encoding.UTF8 will trigger a crash.
    
    These exceptions shouldn't cause the entire repl to crash, so catch them
    and just return the ToString representation in those cases.

--- CSharpRepl.Tests/PrettyPrinterTests.cs
+++ CSharpRepl.Tests/PrettyPrinterTests.cs
@@ -2,6 +2,7 @@
 using CSharpRepl.Services.Roslyn;
 using System.Collections.Generic;
 using static System.Environment;
+using System.Text;
 
 namespace CSharpRepl.Tests;
 
@@ -17,13 +18,14 @@ public class PrettyPrinterTests
 
     public static IEnumerable<object[]> FormatObjectInputs = new[]
     {
-            new object[] { null, false, null },
-            new object[] { null, true, null },
-            new object[] { @"""hello world""", false, @"""\""hello world\"""""},
-            new object[] { @"""hello world""", true, @"""hello world"""},
-            new object[] { "a\nb", false, @"""a\nb"""},
-            new object[] { "a\nb", true, "a\nb"},
-            new object[] { new[] { 1, 2, 3 }, false, "int[3] { 1, 2, 3 }"},
-            new object[] { new[] { 1, 2, 3 }, true, $"int[3] {"{"}{NewLine}  1,{NewLine}  2,{NewLine}  3{NewLine}{"}"}{NewLine}"},
-        };
+        new object[] { null, false, null },
+        new object[] { null, true, null },
+        new object[] { @"""hello world""", false, @"""\""hello world\"""""},
+        new object[] { @"""hello world""", true, @"""hello world"""},
+        new object[] { "a\nb", false, @"""a\nb"""},
+        new object[] { "a\nb", true, "a\nb"},
+        new object[] { new[] { 1, 2, 3 }, false, "int[3] { 1, 2, 3 }"},
+        new object[] { new[] { 1, 2, 3 }, true, $"int[3] {"{"}{NewLine}  1,{NewLine}  2,{NewLine}  3{NewLine}{"}"}{NewLine}"},
+        new object[] { Encoding.UTF8, true, "System.Text.UTF8Encoding+UTF8EncodingSealed"},
+    };
 }
