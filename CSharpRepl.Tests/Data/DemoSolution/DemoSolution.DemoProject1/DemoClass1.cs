8a2e2acdf59db4fe4e8486acae162de6aee312a3
ejesus <ejesus@DESKTOP-EFQFQPL>
2022-05-11 01:16:23 -0300

    Adds DemoSolution for testing purposes

---
+++ CSharpRepl.Tests/Data/DemoSolution/DemoSolution.DemoProject1/DemoClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DemoSolution.DemoProject1
+{
+    public class DemoClass1
+    {
+        public int Add(int a, int b) => a + b;
+    }
+}
---
+++ CSharpRepl.Tests/Data/DemoSolution/DemoSolution.DemoProject2/DemoClass2.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DemoSolution.DemoProject2
+{
+    public class DemoClass2
+    {
+        public int Add(int a, int b) => a + b;
+    }
+}
