9ea22f4def60aecb445d4d4ed39169623056e37f
ejesus <ejesus@DESKTOP-EFQFQPL>
2022-05-14 11:12:21 -0300

    chore: Adding license header to new files

--- CSharpRepl.Services/Dotnet/DotnetBuilder.cs
+++ CSharpRepl.Services/Dotnet/DotnetBuilder.cs
@@ -1,4 +1,8 @@
-﻿using PrettyPrompt.Consoles;
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using PrettyPrompt.Consoles;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
--- CSharpRepl.Services/Roslyn/MetadataResolvers/SolutionFileMetadataResolver.cs
+++ CSharpRepl.Services/Roslyn/MetadataResolvers/SolutionFileMetadataResolver.cs
@@ -1,4 +1,8 @@
-﻿using CSharpRepl.Services.Dotnet;
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using CSharpRepl.Services.Dotnet;
 using Microsoft.CodeAnalysis;
 using PrettyPrompt.Consoles;
 using System;
