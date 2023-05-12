78b48a964fa7eab68591978aab3cb0d2e7ce0195
Will Fuqua <wafuqua@gmail.com>
2023-05-12 20:54:12 +0700

    handle nuget package versions with implicit minor versions

--- CSharpRepl.Tests/NugetPackageInstallerTests.cs
+++ CSharpRepl.Tests/NugetPackageInstallerTests.cs
@@ -66,4 +66,17 @@ public class NugetPackageInstallerTests : IAsyncLifetime
         Assert.NotNull(reference);
         Assert.True(reference.FilePath.Contains("lib", StringComparison.OrdinalIgnoreCase));
     }
+
+
+    [Fact] // https://github.com/waf/CSharpRepl/issues/251
+    public async Task InstallPackageThatContainsImplicitVersioning()
+    {
+        // The package version is "1.2" but if we accidentally normalize the version, it normalizes to 1.2.0 and causes directory not found errors
+        var references = await installer.InstallAsync("Emik.Results", "1.2");
+
+        Assert.True(references.Length >= 1);
+        var reference = references.FirstOrDefault(r => r.FilePath.EndsWith("Emik.Results.dll", StringComparison.OrdinalIgnoreCase));
+        Assert.NotNull(reference);
+        Assert.True(reference.FilePath.Contains("lib", StringComparison.OrdinalIgnoreCase));
+    }
 }
