838197c025360808142c44124d75870c84449628
Will Fuqua <wafuqua@gmail.com>
2026-06-14 14:03:23 +0700

    Nitpick: ensure files have license headers

--- Tests/CSharpRepl.Tests/ReadEvalPrintLoopTests.cs
+++ Tests/CSharpRepl.Tests/ReadEvalPrintLoopTests.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
